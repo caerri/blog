f06fe42eb731e01a35e6dd6f89624eca4dc52d03
Carrie Snow <84377050+caerri@users.noreply.github.com>
2025-11-15 17:59:51 -0700

    feat(blog_posts): implement full CRUD controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,5 +1,5 @@
 class BlogPostsController < ApplicationController
-  before_action :set_blog_post, only: (:show, :edit, :update, :destroy)
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @blog_posts = BlogPost.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DLS in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
   resources :blog_posts
-
-  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  # patch "/blog_posts/:id", to: "blog_posts#update"
-  # delete "/blog_posts/:id", to: "blog_posts#destroy"
-  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
   root "blog_posts#index"
 end
